3d04e06c9c56c153007ffe158866f37d49843024
gkaan <gkaan@daum.net>
2021-11-22 14:50:42 +0900

    [21. 11. 19 # MEMBER APP 1일차]

---
+++ src/main/java/com/spring/mvc/member/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.spring.mvc.member.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class MemberService {
+
+}
